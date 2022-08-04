21363aeb37604a798ca8ba0824f596f3c9ceea85
Matt Rauscher <rauscher.matthieu@gmail.com>
2022-08-04 15:29:49 +0200

    Working on Udacity - ScrollTo to do.

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -29,8 +29,15 @@
  * Start Helper Functions
  * 
 */
-
-
+function isInViewPort(element){
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >=0 &&
+        rect.bottom <= ( window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)  
+    );
+};
 
 /**
  * End Helper Functions
@@ -39,13 +46,52 @@
 */
 
 // build the nav
+const navFragment = document.createDocumentFragment();
+const sections = document.getElementsByTagName('section')
 
+function buildNav(){
+    
+    for(let i = 0; i<sections.length; i++){
+        let sectionText = sections[i].firstElementChild.firstElementChild.innerText;
+        let li = document.createElement("li");
+        li.innerHTML = sectionText;
+        li.classList.add('menu__link');
+        li.classList.add(sections[i].id);
+
+        navFragment.appendChild(li);
+
+    }
+    const navBar = document.getElementById('navbar__list');
+    navBar.appendChild(navFragment);
+
+}
 
 // Add class 'active' to section when near top of viewport
 
+function addActiveWhenViewport(){
+    for (const section of sections){
+        console.log('viewport '+isInViewPort(section))
+        isInViewPort(section) ? 
+        section.classList.add('your-active-class') :
+        section.classList.remove('your-active-class');
+    };
+};
 
 // Scroll to anchor ID using scrollTO event
 
+
+function scrollToAnchor(anchor){
+    const rect = anchor.getBoundingClientRect();
+
+    scrollTo(rect.x, rect.y);
+}
+const navSections = document.getElementById('navbar__list');
+for (navSection in navSections){
+    document.addEventListener("click", function(){
+        navSection.classList
+
+    })
+}
 
 /**
  * End Main Functions
@@ -54,9 +100,13 @@
 */
 
 // Build menu 
-
+buildNav();
 // Scroll to section on link click
+
+
 
 // Set sections as active
 
-
+document.addEventListener("scroll", function(){
+    addActiveWhenViewport()
+});
